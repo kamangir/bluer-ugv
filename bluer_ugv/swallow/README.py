508664bfddb59795716d31b863498996c595968a
kamangir <arash@kamangir.net>
2025-06-10 08:37:57 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/swallow/README.py
+++ bluer_ugv/swallow/README.py
@@ -3,6 +3,7 @@ dict_of_images = {
     "../../diagrams/bluer-swallow/3d-design.png": "../../diagrams/bluer-swallow/3d-design.stl",
     "https://github.com/kamangir/assets2/blob/main/bluer-swallow/20250605_180136.jpg?raw=true": "",
     "https://github.com/kamangir/assets2/blob/main/bluer-swallow/20250608_144453.jpg?raw=true": "",
+    "https://github.com/kamangir/assets2/blob/main/bluer-swallow/20250609_164433.jpg?raw=true": "",
 }
 
 items = [
