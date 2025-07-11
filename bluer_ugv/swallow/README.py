6582f80abec74723d9859539644aadaf712a5b9d
kamangir <arash@kamangir.net>
2025-07-11 11:26:16 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/swallow/README.py
+++ bluer_ugv/swallow/README.py
@@ -28,7 +28,7 @@ dict_of_images = {
     f"{assets}/2025-07-09-10-26-30-itpbmu/grid.png?raw=true": "../docs/bluer-swallow-dataset-generation.md",
     f"{assets2}/lab.png?raw=true": "",
     f"{assets2}/20250709_111955.jpg?raw=true": "",
-    f"{assets2}/2025-07-09-11-20-27-4qf255-000.png?raw=true": "",
+    f"{assets2}/2025-07-09-11-20-27-4qf255-000-2.png?raw=true": "",
     f"{assets}/swallow-dataset-2025-07-11-10-53-04-n3oybs/grid.png?raw=true": "../docs/bluer-swallow-dataset-combination.md",
 }
 
