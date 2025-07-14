3d1f23b84d17c8c66bce37a7bcb0329bb1220213
kamangir <arash@kamangir.net>
2025-07-14 13:44:59 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/__init__.py
+++ bluer_ugv/__init__.py
@@ -4,7 +4,7 @@ ICON = "🐬"
 
 DESCRIPTION = f"{ICON} AI x ROS."
 
-VERSION = "6.392.1"
+VERSION = "6.393.1"
 
 REPO_NAME = "bluer-ugv"
 
--- bluer_ugv/swallow/README.py
+++ bluer_ugv/swallow/README.py
@@ -35,11 +35,17 @@ dict_of_images = {
     f"{assets}/swallow-model-2025-07-11-15-04-03-2glcch/loss.png?raw=true": "./digital/model/validation.md",
     f"{assets}/swallow-model-2025-07-11-15-04-03-2glcch/evaluation.png?raw=true": "./digital/model/validation.md",
     f"{assets}/swallow-model-2025-07-11-15-04-03-2glcch/confusion_matrix.png?raw=true": "./digital/model/validation.md",
+    #
     f"{assets2}/2025-07-09-11-18-07-azy27w.png?raw=true": "https://github.com/kamangir/bluer-algo/blob/main/bluer_algo/docs/image_classifier/dataset/sequence.md",
     f"{assets}/sequence-2025-07-12-21-58-04-0wmt6d/grid.png?raw=true": "https://github.com/kamangir/bluer-algo/blob/main/bluer_algo/docs/image_classifier/dataset/sequence.md",
+    "": "",
+    #
+    f"{assets}/swallow-model-2025-07-14-09-35-16-7q7h82/loss.png?raw=true": "./digital/model/validation.md",
+    f"{assets}/swallow-model-2025-07-14-09-35-16-7q7h82/evaluation.png?raw=true": "./digital/model/validation.md",
+    f"{assets}/swallow-model-2025-07-14-09-35-16-7q7h82/confusion_matrix.png?raw=true": "./digital/model/validation.md",
 }
 
 items = [
-    "[![image]({})]({})".format(image, url if url else image)
+    "" if not image else "[![image]({})]({})".format(image, url if url else image)
     for image, url in dict_of_images.items()
 ]
