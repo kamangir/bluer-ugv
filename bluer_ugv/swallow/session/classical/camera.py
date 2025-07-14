5ed37f42d1eddedfad63861013441a9fd38e4941
kamangir <arash@kamangir.net>
2025-07-14 16:56:37 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/swallow/session/classical/camera.py
+++ bluer_ugv/swallow/session/classical/camera.py
@@ -135,6 +135,7 @@ class ClassicalCamera:
 
     def update(self) -> bool:
         if self.setpoint.speed <= 0:
+            self.buffer = []
             return True
 
         if self.keyboard.mode == OperationMode.PREDICTION:
@@ -147,7 +148,6 @@ class ClassicalCamera:
 
     def update_prediction(self) -> bool:
         if not self.prediction_timer.tick():
-            self.buffer = []
             return True
 
         self.leds.leds["red"]["state"] = not self.leds.leds["red"]["state"]
