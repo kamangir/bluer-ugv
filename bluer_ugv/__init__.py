753f8760bc9cb64b50e1858c16b8e1e0ca2d0cf1
kamangir <arash@kamangir.net>
2025-07-16 14:41:29 +0330

    start of remote-targetting-2025-07-16-3l98ct 🌀 - kamangir/bolt#746

--- bluer_ugv/__init__.py
+++ bluer_ugv/__init__.py
@@ -4,7 +4,7 @@ ICON = "🐬"
 
 DESCRIPTION = f"{ICON} AI x ROS."
 
-VERSION = "6.431.1"
+VERSION = "6.432.1"
 
 REPO_NAME = "bluer-ugv"
 
--- bluer_ugv/swallow/session/classical/camera/tracking.py
+++ bluer_ugv/swallow/session/classical/camera/tracking.py
@@ -1,4 +1,45 @@
+from typing import Tuple
+
+from bluer_sbc.imager.camera import instance as camera
+from bluer_algo.tracker.classes.target import Target
+
 from bluer_ugv.swallow.session.classical.camera.generic import ClassicalCamera
+from bluer_ugv.swallow.session.classical.camera.generic import ClassicalCamera
+from bluer_ugv.swallow.session.classical.keyboard import ClassicalKeyboard
+from bluer_ugv.swallow.session.classical.leds import ClassicalLeds
+from bluer_ugv.swallow.session.classical.setpoint import ClassicalSetPoint
+from typing import Tuple
 
 
-class ClassicalTrackingCamera(ClassicalCamera): ...
+class ClassicalTrackingCamera(ClassicalCamera):
+    def __init__(
+        self,
+        keyboard: ClassicalKeyboard,
+        leds: ClassicalLeds,
+        setpoint: ClassicalSetPoint,
+        object_name: str,
+    ):
+        super().__init__(keyboard, leds, setpoint, object_name)
+
+        self.track_window: Tuple[int, int, int, int] = None
+
+    def initialize(self) -> bool:
+        if not super().initialize():
+            return False
+
+        success, image = camera.capture(
+            close_after=False,
+            open_before=False,
+            log=True,
+        )
+        if not success:
+            return success
+
+        success, self.track_window = Target.select(
+            image,
+            local=False,
+        )
+        if not success:
+            return success
+
+        return True
