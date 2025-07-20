2c495d6c6e2a778736930510113370325eb9e6e1
kamangir <arash@kamangir.net>
2025-07-20 21:30:45 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/parts.py
+++ bluer_ugv/parts.py
@@ -47,6 +47,7 @@ db_of_parts: Dict[str, List[str]] = {
 
 def get_list_of_parts(
     dict_of_parts: Dict[str, str],
+    reference: str = "../parts",
 ) -> Tuple[bool, List[str]]:
     logger.info(
         "{}.get_list_of_parts: {}".format(
@@ -63,9 +64,10 @@ def get_list_of_parts(
     return True, sorted(
         [
             (
-                "1. {}{}.".format(
+                "1. [{}{}]().".format(
                     db_of_parts[part_name][0],
                     ": {}".format(description) if description else "",
+                    f"{reference}/{part_name}.md",
                 )
             )
             for part_name, description in dict_of_parts.items()
