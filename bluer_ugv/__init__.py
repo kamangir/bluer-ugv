816801a4dc9a0573a5bfe1ad3f969a87681c83a3
kamangir <arash@kamangir.net>
2025-07-23 10:28:08 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/__init__.py
+++ bluer_ugv/__init__.py
@@ -4,7 +4,7 @@ ICON = "🐬"
 
 DESCRIPTION = f"{ICON} AI x ROS."
 
-VERSION = "6.505.1"
+VERSION = "6.506.1"
 
 REPO_NAME = "bluer-ugv"
 
--- bluer_ugv/sparrow/README.py
+++ bluer_ugv/sparrow/README.py
@@ -5,6 +5,8 @@ dict_of_images = {
     f"{assets2}/20250713_172325.jpg?raw=true": "",
     f"{assets2}/20250713_172413.jpg?raw=true": "",
     f"{assets2}/20250713_172442_1.gif?raw=true": "",
+    f"{assets2}/20250723_095022.jpg?raw=true": "",
+    f"{assets2}/20250723_095155~2_1.gif?raw=true": "",
 }
 
 items = [
