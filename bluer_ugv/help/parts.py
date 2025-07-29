033f1b5c954f487ad5258024c078e4029e8a0fe4
kamangir <arash@kamangir.net>
2025-07-29 15:51:58 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/help/parts.py
+++ bluer_ugv/help/parts.py
@@ -1,10 +1,8 @@
 from typing import List
 
-from bluer_options.terminal import show_usage, xtra
-from bluer_ai.help.generic import help_functions as generic_help_functions
+from bluer_options.terminal import show_usage
 
-from bluer_plugin import ALIAS
-from bluer_plugin.help.node.functions import help_functions as help_node
+from bluer_ugv import ALIAS
 
 
 def help_adjust(
