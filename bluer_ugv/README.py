51f8ab8d2c9565081a5f76186f8a82da6e9e87d3
kamangir <arash@kamangir.net>
2025-07-29 08:52:58 +0330

    ++ - kamangir/bolt#746 - no-workflow 🪄

--- bluer_ugv/README.py
+++ bluer_ugv/README.py
@@ -38,7 +38,7 @@ items = README.Items(
         },
         {
             "name": "bluer_eagle",
-            "marquee": "https://github.com/kamangir/assets2/raw/main/bluer-eagle/20250726_171953.jpg?raw=true",
+            "marquee": "https://github.com/kamangir/assets2/raw/main/bluer-eagle/file_0000000007986246b45343b0c06325dd.png?raw=true",
             "description": "a remotely controlled ballon.",
             "url": "./bluer_ugv/docs/bluer_eagle",
         },
--- bluer_ugv/eagle/README.py
+++ bluer_ugv/eagle/README.py
@@ -1,8 +1,9 @@
 assets2 = "https://github.com/kamangir/assets2/blob/main/bluer-eagle"
 
 dict_of_images = {
-    f"{assets2}/20250726_171953.jpg?raw=true": "",
     f"{assets2}/20250727_182113.jpg?raw=true": "",
+    f"{assets2}/20250726_171953.jpg?raw=true": "",
+    f"{assets2}/file_0000000007986246b45343b0c06325dd.png?raw=true": "",
 }
 
 items = [
