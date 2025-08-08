7008679a05eb1b1c9f944686684b15e95a9ca086
kamangir <arash@kamangir.net>
2025-08-08 13:48:07 +0330

    start of latest-robins-2025-08-08-36tj2i 🌀 - kamangir/bolt#746

--- bluer_ugv/README/shield.py
+++ bluer_ugv/README/shield.py
@@ -7,11 +7,11 @@ assets2_swallow = "https://github.com/kamangir/assets2/blob/main/bluer-swallow"
 items = ImageItems(
     {
         "https://github.com/kamangir/bluer-ugv/raw/main/diagrams/bluer_swallow/digital.png": "https://github.com/kamangir/bluer-ugv/blob/main/diagrams/bluer_swallow/digital.svg",
-        f"{assets2_swallow}/20250609_164433.jpg?raw=true": "",
-        f"{assets2_swallow}/20250614_102301.jpg?raw=true": "",
-        f"{assets2_swallow}/20250614_114954.jpg?raw=true": "",
-        f"{assets2_swallow}/20250615_192339.jpg?raw=true": "",
-        f"{assets2_swallow}/20250703_153834.jpg?raw=true": "",
+        f"{assets2_swallow}/20250609_164433.jpg": "",
+        f"{assets2_swallow}/20250614_102301.jpg": "",
+        f"{assets2_swallow}/20250614_114954.jpg": "",
+        f"{assets2_swallow}/20250615_192339.jpg": "",
+        f"{assets2_swallow}/20250703_153834.jpg": "",
     }
 )
 
