c45fd4ce0725259d6ba2b226ea05df97f287f29c
kamangir <arash@kamangir.net>
2025-08-17 14:25:56 +0330

    start of parts-update-2025-08-17-rqrhpc 🌀 - kamangir/bolt#746

--- bluer_ugv/swallow/parts.py
+++ bluer_ugv/swallow/parts.py
@@ -10,7 +10,7 @@ dict_of_parts = {
     "330-ohm": "",
     "LED": "",
     "rpi-camera": "",
-    "PCB-single-14x9_5": "",
+    "PCB-single-14x9_5": "2 x",
     "PCB-double-9x7": "",
     "pushbutton": "",
 }
