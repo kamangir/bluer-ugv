302777f72f1cf7c42866fe7ca074784e685fc26e
kamangir <arash@kamangir.net>
2025-09-06 01:37:57 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/__init__.py
+++ bluer_ugv/__init__.py
@@ -6,7 +6,7 @@ ICON = "🐬"
 
 DESCRIPTION = f"{ICON} AI x UGV."
 
-VERSION = "6.774.1"
+VERSION = "6.775.1"
 
 REPO_NAME = "bluer-ugv"
 
--- bluer_ugv/sparrow/README.py
+++ bluer_ugv/sparrow/README.py
@@ -15,5 +15,6 @@ items = ImageItems(
         f"{assets2_bluer_sparrow}/VID-20250830-WA0000~3_1.gif": "",
         f"{assets2_bluer_sparrow}/20250901_105247.jpg": "",
         f"{assets2_bluer_sparrow}/20250905_120625.jpg": "",
+        f"{assets2_bluer_sparrow}/VID-20250905-WA0014_1.gif": "",
     }
 )
