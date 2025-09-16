ee58c9cc16acb418823321e3bc38e522f4e8d6db
kamangir <arash@kamangir.net>
2025-09-16 17:25:18 +0330

    ++ - kamangir/bolt#746 - no-workflow 🪄

--- bluer_ugv/swallow/session/classical/camera/yolo.py
+++ bluer_ugv/swallow/session/classical/camera/yolo.py
@@ -145,8 +145,8 @@ class ClassicalYoloCamera(ClassicalCamera):
 
         detection = metadata["detections"][0]
         logger.info("confidence: {:.2f}".format(detection["confidence"]))
-        detection_y_center = (detection["bbox_xyxy"][1] + detection["bbox_xyxy"][3]) / 2
-        if detection_y_center > image.shape[0] / 2:
+        detection_x_center = (detection["bbox_xyxy"][0] + detection["bbox_xyxy"][2]) / 2
+        if detection_x_center > image.shape[1] / 2:
             self.setpoint.put(
                 what="steering",
                 value=env.BLUER_UGV_SWALLOW_STEERING_SETPOINT,
