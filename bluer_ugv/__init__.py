950246af44b163454a5f2e541a284163db1e9061
kamangir <arash@kamangir.net>
2025-09-29 23:26:10 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/__init__.py
+++ bluer_ugv/__init__.py
@@ -6,7 +6,7 @@ ICON = "🐬"
 
 DESCRIPTION = f"{ICON} AI x UGV."
 
-VERSION = "6.965.1"
+VERSION = "6.966.1"
 
 REPO_NAME = "bluer-ugv"
 
--- bluer_ugv/swallow/README.py
+++ bluer_ugv/swallow/README.py
@@ -1,18 +1,22 @@
 from bluer_objects.README.items import ImageItems
 
-from bluer_ugv.README.consts import assets, assets2_bluer_swallow
+from bluer_ugv.README.consts import (
+    assets,
+    assets2_bluer_swallow,
+    bluer_swallow_mechanical_design,
+)
 
 items = ImageItems(
     {
         f"{assets}/bluer-ugv/bluer-light.png": "",
-        "../../../diagrams/bluer_swallow/3d-design.png": "../../../diagrams/bluer_swallow/3d-design.stl",
+        f"{bluer_swallow_mechanical_design}/3d-design.png": f"{bluer_swallow_mechanical_design}/3d-design.stl",
         f"{assets2_bluer_swallow}/20250605_180136.jpg": "",
         f"{assets2_bluer_swallow}/20250608_144453.jpg": "",
         f"{assets2_bluer_swallow}/20250616_134654.jpg": "",
         f"{assets2_bluer_swallow}/20250616_145049.jpg": "",
         f"{assets2_bluer_swallow}/20250618_102816~2_1.gif": "",
         f"{assets2_bluer_swallow}/20250618_122604.jpg": "",
-        "../../../diagrams/bluer_swallow/cover.png": "../../../diagrams/bluer_swallow/cover.stl",
+        f"{bluer_swallow_mechanical_design}cover.png": f"{bluer_swallow_mechanical_design}/cover.stl",
         f"{assets2_bluer_swallow}/20250629_123616.jpg": "",
         f"{assets2_bluer_swallow}/20250630_214923.jpg": "",
         f"{assets2_bluer_swallow}/20250701_2206342_1.gif": "",
