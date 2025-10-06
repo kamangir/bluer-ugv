cc2399fe76801a7aa3316319d6affcea35562955
kamangir <arash@kamangir.net>
2025-10-06 20:17:08 +0330

    start of rangin-design-2025-10-06-lzpr3h 🌀 - kamangir/bolt#746

--- bluer_ugv/README/rangin/__init__.py
+++ bluer_ugv/README/rangin/__init__.py
@@ -11,7 +11,7 @@ docs = [
         "path": "../docs/rangin/parts.md",
         "items": db_of_parts.as_images(
             dict_of_parts,
-            reference="../parts",
+            reference="./parts",
         ),
         "macros": {
             "parts:::": db_of_parts.as_list(
