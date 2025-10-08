4ec98334fc58daf29ca83e1d6521203c2631925d
kamangir <arash@kamangir.net>
2025-10-08 11:20:19 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/README/shield.py
+++ bluer_ugv/README/shield.py
@@ -25,6 +25,7 @@ items = ImageItems(
         f"{swallow_assets2}/20251002_103720.jpg": "",
         f"{swallow_assets2}/design/v5/01.jpg": "",
         f"{swallow_assets2}/20251008_105016.jpg": "",
+        f"{swallow_electrical_designs}/nuts-bolts-spacers.png": f"{swallow_electrical_designs}/nuts-bolts-spacers.svg",
     }
 )
 
--- bluer_ugv/__init__.py
+++ bluer_ugv/__init__.py
@@ -4,7 +4,7 @@ ICON = "🐬"
 
 DESCRIPTION = f"{ICON} AI x UGV."
 
-VERSION = "7.35.1"
+VERSION = "7.36.1"
 
 REPO_NAME = "bluer-ugv"
 
