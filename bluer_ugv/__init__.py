870a195eb7641f41fe452b57da9a8c5716e8aca9
kamangir <arash@kamangir.net>
2025-10-08 11:25:34 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/__init__.py
+++ bluer_ugv/__init__.py
@@ -4,7 +4,7 @@ ICON = "🐬"
 
 DESCRIPTION = f"{ICON} AI x UGV."
 
-VERSION = "7.38.1"
+VERSION = "7.39.1"
 
 REPO_NAME = "bluer-ugv"
 
--- bluer_ugv/swallow/parts.py
+++ bluer_ugv/swallow/parts.py
@@ -5,5 +5,5 @@ dict_of_parts = {
     "on-off-switch": "",
     "charging-port": "",
     "connector": "4 pairs",
-    "nuts-bolts-spacers": "TBA",
+    "nuts-bolts-spacers": "M2: (2 x bolt + 5 x 5 mm + 4 x nut) + M2.5: (4 x bolt + 8 x 10 mm + 4 x nut) + M3: (1 x bolt + 4 x 35 mm + 3 x 25 mm + 9 x 15 mm + 4 x 5 mm + 5 x nut)",
 }
