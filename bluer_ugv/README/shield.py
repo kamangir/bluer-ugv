0b39504fc9b5a15b8fde80191f950f77e07c0fd0
kamangir <arash@kamangir.net>
2025-10-09 19:40:40 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/README/shield.py
+++ bluer_ugv/README/shield.py
@@ -2,6 +2,7 @@ from bluer_objects.README.items import ImageItems
 
 from bluer_ugv.README.swallow.consts import (
     swallow_assets2,
+    swallow_designs,
     swallow_electrical_designs,
 )
 
@@ -29,6 +30,7 @@ items = ImageItems(
         f"{swallow_assets2}/20251008_133418.jpg": "",
         f"{swallow_assets2}/20251008_124129.jpg": "",
         f"{swallow_assets2}/20251008_124932.jpg": "",
+        f"{swallow_designs}/kicad/swallow/swallow.png": f"{swallow_designs}/kicad/swallow/swallow.pdf",
     }
 )
 
