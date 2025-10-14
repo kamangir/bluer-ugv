ceedf322b2acdfc05d35a627791b95191bdfb27f
kamangir <arash@kamangir.net>
2025-10-14 21:40:06 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/__init__.py
+++ bluer_ugv/__init__.py
@@ -4,7 +4,7 @@ ICON = "🐬"
 
 DESCRIPTION = f"{ICON} AI x UGV."
 
-VERSION = "7.140.1"
+VERSION = "7.141.1"
 
 REPO_NAME = "bluer-ugv"
 
--- bluer_ugv/designs/ravin/ravin4/parts.py
+++ bluer_ugv/designs/ravin/ravin4/parts.py
@@ -5,7 +5,16 @@ dict_of_parts = {
     "yellow-gearbox-dc-motor": "4 x 270 RPM",
     "yellow-wheels": "x 4",
     "L-1x2": "x 8",
-    "nuts-bolts-spacers": "M3: (TBA x bolt + TBA x TBA mm + TBA x nut)",
+    "nuts-bolts-spacers": "M3: ({})".format(
+        " + ".join(
+            [
+                "10 x 6 mm bolt",
+                "8 x 25 mm bolt",
+                "2 x 5 mm spacer",
+                "20 x nut",
+            ]
+        )
+    ),
     "ni-mh-battery": "x 4",
     "mt-3608": "",
 }
