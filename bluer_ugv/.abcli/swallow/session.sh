8078b110aa4b0b3210f004e1c7d1008dd6df0e1c
kamangir <arash@kamangir.net>
2025-10-31 11:33:08 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/.abcli/swallow/session.sh
+++ bluer_ugv/.abcli/swallow/session.sh
@@ -40,7 +40,7 @@ function bluer_ugv_swallow_session() {
         bluer_ai_log "🏓 stopping bps ..."
 
         chmod 700 \
-            $ABCLI_OBJECT_ROOT/$object_name/metadata.yaml
+            $ABCLI_OBJECT_ROOT/$abcli_object_name/metadata.yaml
 
         bluer_ai_eval dryrun=$do_dryrun \
             bluer_algo_bps \
