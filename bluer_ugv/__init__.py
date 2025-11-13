07b703b80a7c97b91fe72cca7a2f0ba02f68834a
kamangir <arash@kamangir.net>
2025-11-13 19:28:44 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/__init__.py
+++ bluer_ugv/__init__.py
@@ -4,7 +4,7 @@ ICON = "🐬"
 
 DESCRIPTION = f"{ICON} AI x UGV."
 
-VERSION = "7.312.1"
+VERSION = "7.313.1"
 
 REPO_NAME = "bluer-ugv"
 
--- bluer_ugv/designs/rangin/parts.py
+++ bluer_ugv/designs/rangin/parts.py
@@ -6,10 +6,9 @@ dict_of_parts = {
     "dc-switch": "60 - 100 A @ 12 V DC",
     "power-adapter": "12V DC, 1 A",
     "TV-bracket": "2 x",
-    #
     "dc-volt-meter": "0 - 50 V, 0 - 50 A",
+    #
     "mcb": "2 - 3 A @ 220 V AC",
-    "ac-switch": "2 - 5 A @ 250 V AC",
     "safety-fuse": "30 mA trip",
     "ac-volt-meter": "220 V, 5 A",
     "emergency-stop": "100 A DC or relay-controlled",
