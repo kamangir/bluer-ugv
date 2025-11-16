7685d0441b263dd9db595c0ecf1643d044917765
kamangir <arash@kamangir.net>
2025-11-16 14:28:01 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/__init__.py
+++ bluer_ugv/__init__.py
@@ -4,7 +4,7 @@ ICON = "🐬"
 
 DESCRIPTION = f"{ICON} AI x UGV."
 
-VERSION = "7.328.1"
+VERSION = "7.329.1"
 
 REPO_NAME = "bluer-ugv"
 
--- bluer_ugv/docs/rangin/specs-template.md
+++ bluer_ugv/docs/rangin/specs-template.md
@@ -2,4 +2,3 @@ title:::
 
 - speed: gearbox output == 60 RPM @ 6 VDC on 12 cm wheels == 2.7 km / h @ 12 VDC (max speed).
 - 🔥
-
