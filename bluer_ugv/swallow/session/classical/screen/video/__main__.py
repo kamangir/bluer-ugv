5bc0f3557e1a5d99a4d73a6dab9fdbcbe3c30990
kamangir <arash@kamangir.net>
2025-11-19 15:15:08 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/swallow/session/classical/screen/video/__main__.py
+++ bluer_ugv/swallow/session/classical/screen/video/__main__.py
@@ -91,6 +91,8 @@ if args.task == "play":
         time.sleep(args.timeout)
         success = video_player.stop()
 
+        logger.info('type in "reset" if the prompt is invisible.')
+
 else:
     success = None
 
