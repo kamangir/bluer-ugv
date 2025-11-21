7f942ef0dfca4257f87351b1a1ac17c07a582288
kamangir <arash@kamangir.net>
2025-11-21 14:12:41 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/README/computer/docs.py
+++ bluer_ugv/README/computer/docs.py
@@ -1,3 +1,6 @@
+from bluer_objects.README.items import ImageItems
+
+from bluer_ugv.README.swallow.consts import swallow_assets2
 from bluer_ugv.README.computer import box, pcb, power, schematics, testing
 
 
@@ -5,6 +8,11 @@ docs = (
     [
         {
             "path": "../docs/swallow/digital/design/computer",
+            "items": ImageItems(
+                {
+                    f"{swallow_assets2}/20251121_124335.jpg": "",
+                }
+            ),
         },
         {
             "path": "../docs/swallow/digital/design/computer/naming.md",
