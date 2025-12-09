a4c8b39bb71e23ca617b9b2f42f1632d8e3d9d6f
kamangir <arash@kamangir.net>
2025-12-09 08:45:38 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/.abcli/swallow/git/rm_keys.sh
+++ bluer_ugv/.abcli/swallow/git/rm_keys.sh
@@ -2,7 +2,7 @@
 
 function bluer_ugv_swallow_git_rm_keys() {
     local options=$1
-    local do_dryrun=$(bluer_ai_option_int "$options" dryrun 0)
+    local do_dryrun=$(bluer_ai_option_int "$options" dryrun 1)
 
     if [[ "$abcli_is_rpi" == false ]]; then
         bluer_ai_log_error "only works on rpi."
@@ -10,10 +10,10 @@ function bluer_ugv_swallow_git_rm_keys() {
     fi
 
     bluer_ai_eval dryrun=$do_dryrun \
-        rm -v ~/.ssh/$BLUER_AI_GIT_SSH_KEY_NAME
+        sudo rm -v ~/.ssh/$BLUER_AI_GIT_SSH_KEY_NAME
 
     bluer_ai_eval dryrun=$do_dryrun \
-        rm -v ~/.ssh/$BLUER_AI_GIT_SSH_KEY_NAME.pub
+        sudo rm -v ~/.ssh/$BLUER_AI_GIT_SSH_KEY_NAME.pub
 
     local repo
     for repo in $(bluer_ai_plugins list_of_external --delim space --log 0 --repo_names 1); do
--- bluer_ugv/__init__.py
+++ bluer_ugv/__init__.py
@@ -4,7 +4,7 @@ ICON = "🐬"
 
 DESCRIPTION = f"{ICON} AI x UGV."
 
-VERSION = "7.550.1"
+VERSION = "7.551.1"
 
 REPO_NAME = "bluer-ugv"
 
