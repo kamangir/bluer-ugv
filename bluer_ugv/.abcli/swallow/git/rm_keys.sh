e1fab7125fa56e88da899a7fd63f0e6ca5f3f38c
kamangir <arash@kamangir.net>
2025-12-11 15:42:47 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/.abcli/swallow/git/rm_keys.sh
+++ bluer_ugv/.abcli/swallow/git/rm_keys.sh
@@ -9,11 +9,9 @@ function bluer_ugv_swallow_git_rm_keys() {
         return 1
     fi
 
-    bluer_ai_eval dryrun=$do_dryrun \
-        sudo rm -v ~/.ssh/$BLUER_AI_GIT_SSH_KEY_NAME
+    sudo rm -v ~/.ssh/$BLUER_AI_GIT_SSH_KEY_NAME
 
-    bluer_ai_eval dryrun=$do_dryrun \
-        sudo rm -v ~/.ssh/$BLUER_AI_GIT_SSH_KEY_NAME.pub
+    sudo rm -v ~/.ssh/$BLUER_AI_GIT_SSH_KEY_NAME.pub
 
     local repo
     for repo in $(bluer_ai_plugins list_of_external --delim space --log 0 --repo_names 1); do
