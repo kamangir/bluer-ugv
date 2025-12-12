4c6001ae875efd11263f62a396b232ecc0fc0ee3
kamangir <arash@kamangir.net>
2025-12-12 11:27:01 +0330

    ++ - kamangir/bolt#746

--- bluer_ugv/swallow/session/classical/camera/tracking.py
+++ bluer_ugv/swallow/session/classical/camera/tracking.py
@@ -37,7 +37,7 @@ class ClassicalTrackingCamera(ClassicalCamera):
             log=True,
         )
 
-        self.tracker = get_tracker_class(BLUER_ALGO_TRACKER_DEFAULT_ALGO)
+        _, self.tracker = get_tracker_class(BLUER_ALGO_TRACKER_DEFAULT_ALGO)
 
     def initialize(self) -> bool:
         if not super().initialize():
