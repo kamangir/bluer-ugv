6c7f58784afef4247372e115faffc9b5b171a597
kamangir <arash@kamangir.net>
2025-12-22 14:30:55 +0330

    ++

--- bluer_ugv/.abcli/rangin/init.sh
+++ bluer_ugv/.abcli/rangin/init.sh
@@ -5,10 +5,10 @@ function bluer_ugv_rangin_init() {
     local do_install=$(bluer_ai_option_int "$options" install 0)
 
     if [[ "$do_install" == 1 ]]; then
-        [[ "$abci_is_rpi" == 1 ]] &&
+        [[ "$abcli_is_rpi" == 1 ]] &&
             sudo apt install -y alsa-utils wlr-randr
     fi
 
-    [[ "$abci_is_rpi" == 1 ]] &&
+    [[ "$abcli_is_rpi" == 1 ]] &&
         wlr-randr --output HDMI-A-1 --transform 90
 }
