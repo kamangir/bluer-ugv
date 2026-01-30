fafbc24926d12dcb9ce2081f2f363fe789a0f1b3
kamangir <arash@kamangir.net>
2026-01-30 16:55:24 +0330

    ++

--- bluer_ugv/__init__.py
+++ bluer_ugv/__init__.py
@@ -4,7 +4,7 @@ ICON = "🐬"
 
 DESCRIPTION = f"{ICON} AI x UGV."
 
-VERSION = "7.828.1"
+VERSION = "7.829.1"
 
 REPO_NAME = "bluer-ugv"
 
--- bluer_ugv/swallow/session/classical/setpoint/classes.py
+++ bluer_ugv/swallow/session/classical/setpoint/classes.py
@@ -89,12 +89,19 @@ class ClassicalSetPoint:
 
     def put(
         self,
-        value: Union[int, bool],
-        what: str,
+        value: Union[int, bool, Dict[str, Union[int, bool]]],
+        what: str = "all",
         log: bool = True,
         steering_expires_in: float = 0,
     ):
         with self._lock:
+            if what == "all":
+                self.speed = min(100, max(-100, int(value["speed"])))
+                self.started = bool(value["started"])
+                self.steering = min(100, max(-100, int(value["steering"])))
+                self.steering_expiry = time.time() + steering_expires_in
+                return
+
             if what == "speed":
                 self.speed = min(100, max(-100, int(value)))
                 if log:
