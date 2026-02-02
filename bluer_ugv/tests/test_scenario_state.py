bfcb8e8c0f80389ec4842d58a03fbc190b10029b
kamangir <arash@kamangir.net>
2026-02-02 22:00:49 +0330

    pipeline fix, hopefully

--- bluer_ugv/tests/test_scenario_state.py
+++ bluer_ugv/tests/test_scenario_state.py
@@ -1,7 +1,7 @@
-from bluer_ugv.swallow.session.classical.scenario.state.types.generic import State
+from bluer_ugv.swallow.session.classical.scenario.state.generic import GenericState
 
 
 def test_scenario_state():
-    state = State("some name")
+    state = GenericState("some name")
     assert state.name
     assert isinstance(state.params, dict)
