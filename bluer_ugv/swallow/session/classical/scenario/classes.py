eef98aa87dcc670bc274d9d4a277b9fb3fe79e34
kamangir <arash@kamangir.net>
2026-02-03 16:59:56 +0330

    wip

--- bluer_ugv/swallow/session/classical/scenario/classes.py
+++ bluer_ugv/swallow/session/classical/scenario/classes.py
@@ -27,7 +27,10 @@ class ClassicalScenario:
             self.state_machine = dict_of_state_machines.get(
                 env.BLUER_UGV_SWALLOW_SCENARIO,
                 GenericStateMachine,
-            )(keyboard=self.keyboard)
+            )(
+                keyboard=self.keyboard,
+                camera=self.camera,
+            )
 
     def update(self) -> bool:
         with self._lock:
