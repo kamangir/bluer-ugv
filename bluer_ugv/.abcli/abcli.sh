b6d68a0f86799db07bf5ae208aff5c6deaa7a717
kamangir <arash@kamangir.net>
2026-02-14 13:24:12 +0330

    wip

--- bluer_ugv/.abcli/abcli.sh
+++ bluer_ugv/.abcli/abcli.sh
@@ -3,7 +3,7 @@
 bluer_ai_source_caller_suffix_path /tests
 
 bluer_ai_env_dot_load \
-    caller,plugin=bluer_ugv,suffix=/../..
+    plugin=bluer_ugv,suffix=/../..
 
 bluer_ai_env_dot_load \
-    caller,filename=config.env,suffix=/..
+    filename=config.env,suffix=/..
