52b9691dae65abe9e358753e3ff1eed85a21916f
kamangir <arash@kamangir.net>
2026-02-18 13:39:36 +0330

    wip

--- bluer_ugv/README/swallow/digital/design/arzhang4/docs.py
+++ bluer_ugv/README/swallow/digital/design/arzhang4/docs.py
@@ -1,15 +1,13 @@
-from bluer_sbc.parts.db import db_of_parts
 from bluer_sbc.README.design import design_doc_parts
 
-from bluer_ugv.README.swallow.digital.design.arzhang4 import tagline, v1
-from bluer_ugv.designs.swallow.parts import dict_of_parts
+from bluer_ugv.README.swallow.digital.design.arzhang4 import tagline, v1, parts
 
 docs = [
     {
         "path": "../docs/swallow/digital/design/arzhang4",
         "macros": {
             **design_doc_parts(
-                dict_of_parts=dict_of_parts,
+                dict_of_parts=parts.dict_of_parts,
                 parts_reference="repo",
             ),
             "tagline:::": tagline,
--- bluer_ugv/README/swallow/digital/design/arzhang4/parts.py
+++ bluer_ugv/README/swallow/digital/design/arzhang4/parts.py
@@ -1,4 +1,4 @@
-parts = {
+dict_of_parts = {
     "brushless-driver": "4 x",
     "scooter-wheel": "4 x",
 }
