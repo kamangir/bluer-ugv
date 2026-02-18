deea78c388f50638b1262ebad6dece8776574cd0
kamangir <arash@kamangir.net>
2026-02-18 13:38:13 +0330

    wip

--- bluer_ugv/README/swallow/digital/design/arzhang4/docs.py
+++ bluer_ugv/README/swallow/digital/design/arzhang4/docs.py
@@ -1,4 +1,5 @@
 from bluer_sbc.parts.db import db_of_parts
+from bluer_sbc.README.design import design_doc_parts
 
 from bluer_ugv.README.swallow.digital.design.arzhang4 import tagline, v1
 from bluer_ugv.designs.swallow.parts import dict_of_parts
@@ -6,17 +7,12 @@ from bluer_ugv.designs.swallow.parts import dict_of_parts
 docs = [
     {
         "path": "../docs/swallow/digital/design/arzhang4",
-        "items": db_of_parts.as_images(
-            dict_of_parts,
-            reference="repo",
-        ),
         "macros": {
-            "tagline:::": tagline,
-            "parts:::": db_of_parts.as_list(
-                dict_of_parts,
-                reference="repo",
-                log=False,
+            **design_doc_parts(
+                dict_of_parts=dict_of_parts,
+                parts_reference="repo",
             ),
+            "tagline:::": tagline,
         },
     },
 ] + v1.docs
