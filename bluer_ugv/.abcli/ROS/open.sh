d7d7592dc5c0ade09d8c1b5c042872e2ed2d3664
kamangir <arash@kamangir.net>
2026-02-19 21:43:44 +0330

    ++

--- bluer_ugv/.abcli/ROS/open.sh
+++ bluer_ugv/.abcli/ROS/open.sh
@@ -3,13 +3,23 @@
 function bluer_ugv_ROS_open() {
     local options=$1
 
+    local machine_type=""
+    [[ "$abcli_is_mac" == true ]] &&
+        machine_type="mac"
+    [[ "$abcli_is_rpi" == true ]] &&
+        machine_type="rpi"
+    if [[ -z "$machine_type" ]]; then
+        bluer_ai_log_error "@ROS: start: machine type not found."
+        return 1
+    fi
+
     bluer_ai_badge - "🦾"
 
     local init_file="/root/git/bluer-ai/bluer_ai/.abcli/bluer_ai.sh"
 
     bluer_ai_eval \
         ,$options \
-        sudo docker exec -it bluer_ugv_ros_rpi \
+        sudo docker exec -it bluer_ugv_ros_$machine_type \
         bash --init-file $init_file
 
     bluer_ai_badge reset
--- bluer_ugv/__init__.py
+++ bluer_ugv/__init__.py
@@ -4,7 +4,7 @@ ICON = "🐬"
 
 DESCRIPTION = f"{ICON} AI 4 UGVs."
 
-VERSION = "7.1095.1"
+VERSION = "7.1096.1"
 
 REPO_NAME = "bluer-ugv"
 
