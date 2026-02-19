f87f08e4469ed789ba46590dd2ae5c06e4b86bba
kamangir <arash@kamangir.net>
2026-02-19 19:44:25 +0330

    wip

---
+++ bluer_ugv/ROS/entrypoint.sh
@@ -0,0 +1,40 @@
+#! /usr/bin/env bash
+
+function entrypoint() {
+    local check_filename=/root/git/entry-completed
+    if [[ -f "$check_filename" ]]; then
+        echo "✅ entrypoint."
+        return 0
+    fi
+
+    sudo apt update
+    sudo apt install -y \
+        python3-venv \
+        python3-pip \
+        build-essential
+
+    pip install --upgrade pip
+
+    python3 -m venv /root/venv/bluer_ai
+    source /root/venv/bluer_ai/bin/activate
+
+    pip install blueness
+
+    local repo_name
+    for repo_name in \
+        bluer-options \
+        bluer-objects \
+        bluer-ai \
+        bluer-algo \
+        bluer-agent \
+        bluer-sbc \
+        bluer-ugv; do
+
+        cd /root/git/$repo_name
+        pip install -e .
+    done
+
+    touch $check_filename
+}
+
+entrypoint
