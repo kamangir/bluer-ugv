aa3b57249d1b3f55fa82dfc7e4bf61a833c3d184
kamangir <arash@kamangir.net>
2026-02-19 18:38:47 +0330

    wip

--- bluer_ugv/ROS/bootstrap.sh
+++ bluer_ugv/ROS/bootstrap.sh
@@ -2,19 +2,19 @@
 
 function runme() {
     if [[ ! -d "/root/venv/bluer_ai" ]]; then
-        echo "venv not found, creating..."
+        echo "📜 venv not found, creating..."
 
         sudo apt update
         sudo apt install -y python3-venv python3-pip
 
         python3 -m venv /root/venv/bluer_ai
+
+        pip install -U pip setuptools wheel
+
+        pip install blueness
     fi
     source /root/venv/bluer_ai/bin/activate
     [[ $? -ne 0 ]] && return 1
-
-    pip install -U pip setuptools wheel
-
-    pip install blueness
 
     local repo_name
     for repo_name in \
@@ -28,7 +28,7 @@ function runme() {
         cd /root/git/$repo_name
         [[ $? -ne 0 ]] && return 1
 
-        echo "installing $repo_name..."
+        echo "📜 installing $repo_name..."
         pip install -e .
         [[ $? -ne 0 ]] && return 1
     done
