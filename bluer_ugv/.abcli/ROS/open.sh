38da4fb1b97a31c02841b43e533b94b2f221b374
kamangir <arash@kamangir.net>
2026-02-19 19:01:50 +0330

    wip

--- bluer_ugv/.abcli/ROS/open.sh
+++ bluer_ugv/.abcli/ROS/open.sh
@@ -8,7 +8,7 @@ function bluer_ugv_ROS_open() {
     bluer_ai_eval \
         ,$options \
         sudo docker exec -it bluer_ugv_ROS \
-        bash --init-file /root/git/bluer-ugv/bluer_ugv/ROS/bootstrap.sh
+        bash --init-file /root/git/bluer-ai/bluer_ai/.abcli/bluer_ai.sh
 
     bluer_ai_badge reset
 }
