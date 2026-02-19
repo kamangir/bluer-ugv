ace5723ba87f2afe52bd958d030ee7b59e3e8ff5
kamangir <arash@kamangir.net>
2026-02-19 16:57:54 +0330

    ++

--- bluer_ugv/.abcli/ROS/install.sh
+++ bluer_ugv/.abcli/ROS/install.sh
@@ -6,6 +6,11 @@ function bluer_ugv_ROS_install() {
     if [[ "$abcli_is_rpi" == true ]]; then
         pushd $abcli_path_temp >/dev/null
         curl -sSL https://get.docker.com | sh
+        if [[ $? -ne 0 ]]; then
+            return 1
+            popd >/dev/null
+        fi
+
         sudo usermod -aG docker $USER
         popd >/dev/null
     fi
