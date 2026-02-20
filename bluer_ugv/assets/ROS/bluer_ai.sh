358d99598fbc1e07f12d7324f14551c037b36d53
kamangir <arash@kamangir.net>
2026-02-20 11:16:06 +0330

    wip

--- bluer_ugv/assets/ROS/bluer_ai.sh
+++ bluer_ugv/assets/ROS/bluer_ai.sh
@@ -38,7 +38,7 @@ function bluer_ugv_ROS_container_init() {
         touch $filename
     fi
 
-    export BLUER_AI_IGNORED_EXTERNAL_PLUGINS="abadpour,bluer-academy,bluer-flow,bluer-geo,bluer-journal,bluer-plugin,bluer-resistance,bluer-south,btc-prediction-bash"
+    export BLUER_AI_IGNORED_EXTERNAL_PLUGINS="abadpour,bluer-academy,bluer-flow,bluer-geo,bluer-journal,bluer-plugin,bluer-resistance,bluer-sandbox,bluer-south,btc-prediction-bash,gizai"
 
     source /root/git/bluer-ai/bluer_ai/.abcli/bluer_ai.sh ~verbose
 
